087457fbea447870ceb39676175c2e6e372bd34b
Kishor <solunkek25@gmail.com>
2025-09-26 22:15:06 +0530

    Test Git

--- src/test/java/testcases/ProductTest.java
+++ src/test/java/testcases/ProductTest.java
@@ -214,4 +214,25 @@ public class ProductTest extends BaseClass
 								
 		}
 		
+		//Test Git
+		
+				@Test
+				public void deleteProduct1() throws Exception 
+				{
+					configreader = new configReader();
+					int productId = configreader.getIntProperty("productId");
+				
+					
+					given()
+						.pathParam("id", productId)
+						
+					.when()
+						.delete(Routes.PRODUCT_DELETE)
+				
+					.then()
+						.statusCode(200)
+						.log().all();
+										
+				}
+		
 }
